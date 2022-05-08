c14cbf200a204d491c442611f926f981f2b4d91f
zhoubichuan <zhoubichuan@icloud.com>
2022-05-09 01:21:13 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -129,7 +129,7 @@ module.exports = {
             '4.mongodb',
             '5.redis',
             '6.mysql',
-            'i18n'
+            '7.express'
           ],
           '/senior/use/': ['1.recursionAndDynamics', '2.extend', '3.componentCommunication', '4.render', '5.api', '6.form', '7.checkBox', '8.active', '9.alert', '10.table', '11.tree'],
           '/senior/npm/': [],
