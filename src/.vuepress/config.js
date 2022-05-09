8e13892e2abbc44ab69929ba4196c747835f4dd8
zhoubichuan <zhoubichuan@icloud.com>
2022-05-10 02:07:06 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,141 +1,133 @@
 module.exports = {
-  // 基础配置
+  title: 'Docker学习笔记',
+  description: '风浪没平息 我宣告奔跑的意义',
   base: '/web-docker/', // 部署站点的基础路径
-  description: 'Docker相关知识点',
-  locales: {
-    // 默认标题
-    '/': {
-      title: 'Docker笔记',
-      description: ''
-    }
-  },
+  port: 3009,
+  dest: 'dist', // 指定 vuepress 的输出目录
   markdown: {
+    toc: { includeLevel: [2, 3] },
     lineNumbers: true // 代码块显示行号
   },
-  head: [
-    // 添加链接 pwa 的 manifest 如果需要
-    [
-      'link',
-      {
-        rel: 'icon',
-        href: ''
-      }
-    ],
-    [
-      'meta',
-      {
-        name: 'apple-mobile-web-app-capable',
-        content: 'yes'
-      }
-    ],
-    [
-      'meta',
-      {
-        name: 'apple-mobile-web-app-status-bar-style',
-        content: 'black'
-      }
-    ],
-    [
-      'meta',
-      {
-        name: 'msapplication-TileColor',
-        content: '#000000'
-      }
-    ]
-  ],
-  port: 3009,
-  dest: 'dist', // 指定 vuepress build 的输出目录
-  serviceWorker: true, // pwa
+  // head: [
+  //   ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
+  //   ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
+  // ],
 
+  plugins: {
+    // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: true
+    },
+    // 名称：@vuepress/plugin-back-to-top 效果：文章看到下面的时候，点击一个图标会回到顶部
+    '@vuepress/back-to-top': true,
+    '@vuepress/medium-zoom': true,
+    '@vuepress/nprogress': true,
+    // 名称：@vuepress/plugin-active-header-links 效果：页面滚动时自动激活侧边栏链接的插件，效果就是右边内容滚动的时候，看到哪里了，左侧菜单会自动高亮显示当前看的目录。
+    '@vuepress/active-header-links': {
+      sidebarLinkSelector: '.sidebar-link',
+      headerAnchorSelector: '.header-anchor'
+    },
+    "@vssue/vuepress-plugin-vssue": {
+      platform: 'github', //v3的platform是github，v4的是github-v4
+      locale: 'zh', //语言
+      // 其他的 Vssue 配置
+      owner: 'zhoubichuan', //github账户名
+      repo: 'web-docker', //github一个项目的名称
+      clientId: 'Iv1.2923ba5d4de48a3c', //注册的Client ID
+      clientSecret: '110210', //注册的Client Secret
+      autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+    },
+    "vuepress-plugin-boxx": ["vuepress-plugin-boxx"]
+  },
   // 主题配置
   themeConfig: {
-    editLinks: true,
+    repo: 'https://github.com/zhoubichuan/web-docker', // 源码地址
+    repoLabel: '查看源码', // (查看源码的)组件名称
+    docsBranch: 'master', // git 源仓库 仓库分支
+    editLinks: true, // 编辑链接
     docsDir: 'src', // 假如文档不是放在仓库的根目录下
-    locales: {
-      '/': {
-        docsBranch: 'master', // 假如文档放在一个特定的分支下
-        editLinks: true, // 启用编辑
-        editLinkText: '在github上编辑此页',
-        lastUpdated: '上次更新', // 获取每个文件最后一次 git 提交的时间戳
-        nav: [{
-          text: '基础知识',
-          items: [{
-            text: '一.docker基础',
-            link: '/base/build/1.index'
-          },
-          {
-            text: '二.docker实践',
-            link: '/base/practice/1.docker',
-          }
-          ]
-        },
-        {
-          text: '高级知识',
-          items: [{
-            text: '一.组件精讲',
-            link: '/senior/use/1.recursionAndDynamics'
-          },
-          {
-            text: '二.TypeScript',
-            link: '/senior/typeScript'
-          },
-          ]
-        },
-        {
-          text: '源码知识',
-          items: [{
-            text: '1.vue源码解析',
-            link: '/source/vue/1.preparation'
-          },
-          {
-            text: '2.vuex源码解析',
-            link: '/source/vuex'
-          },
-          {
-            text: '3.vue-router源码解析',
-            link: '/source/vue-router'
-          },
-          ]
-        }
-        ],
-        sidebar: {
-          '/base/build/': [
-            '1.index',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6',
-            '7',
-            '8',
-            '9',
-            '10',
-            '11',
-            '12',
-            '13',
-            '14',
-            '15',
-            '16',
-            '17',
-            '18',
-            '19',
-            '20',
-            '21'
-          ],
-          '/base/practice/': [
-            '1.docker',
-            '2.Jenkins',
-            '3.gitlab',
-            '4.mongodb',
-            '5.redis',
-            '6.mysql',
-            '7.express'
-          ],
-          '/senior/use/': ['1.recursionAndDynamics', '2.extend', '3.componentCommunication', '4.render', '5.api', '6.form', '7.checkBox', '8.active', '9.alert', '10.table', '11.tree'],
-          '/senior/npm/': [],
-          '/source/vue/': ['1.preparation', '2.dataDriven', '3.componentization', '4.deepResponsePrinciple', '5.compile', '6.extend', '7.vueRouter', '8.vuex'],
-        }
+    editLinkText: '在github上编辑此页',
+    sidebarDepth: 2,
+    lastUpdated: '上次更新', // 获取每个文件最后一次 git 提交的时间戳
+    nav: [{
+      text: '基础知识',
+      items: [{
+        text: '一.docker基础',
+        link: '/base/build/1.index'
+      },
+      {
+        text: '二.docker实践',
+        link: '/base/practice/1.docker',
       }
+      ]
+    },
+    {
+      text: '高级知识',
+      items: [{
+        text: '一.组件精讲',
+        link: '/senior/use/1.recursionAndDynamics'
+      },
+      {
+        text: '二.TypeScript',
+        link: '/senior/typeScript'
+      },
+      ]
+    },
+    {
+      text: '源码知识',
+      items: [{
+        text: '1.vue源码解析',
+        link: '/source/vue/1.preparation'
+      },
+      {
+        text: '2.vuex源码解析',
+        link: '/source/vuex'
+      },
+      {
+        text: '3.vue-router源码解析',
+        link: '/source/vue-router'
+      },
+      ]
     }
+    ],
+    sidebar: {
+      '/base/build/': [
+        '1.index',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21'
+      ],
+      '/base/practice/': [
+        '1.docker',
+        '2.Jenkins',
+        '3.gitlab',
+        '4.mongodb',
+        '5.redis',
+        '6.mysql',
+        '7.express'
+      ],
+      '/senior/use/': ['1.recursionAndDynamics', '2.extend', '3.componentCommunication', '4.render', '5.api', '6.form', '7.checkBox', '8.active', '9.alert', '10.table', '11.tree'],
+      '/senior/npm/': [],
+      '/source/vue/': ['1.preparation', '2.dataDriven', '3.componentization', '4.deepResponsePrinciple', '5.compile', '6.extend', '7.vueRouter', '8.vuex'],
+    },
+    searchMaxSuggestoins: 10
   },
 }
