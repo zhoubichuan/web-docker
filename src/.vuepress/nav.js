cf468ad34ab20259fc323f267231ae060313cd55
周必川 <zhoubichuan@icloud.com>
2023-10-04 23:05:30 +0800

    update

---
+++ src/.vuepress/nav.js
@@ -0,0 +1,38 @@
+const nav = [
+  {
+    text: "基础知识",
+    items: [
+      {
+        text: "一.桌面版实践",
+        link: "/base/1.desktop/1.index",
+      },
+      {
+        text: "二.命令实践",
+        link: "/base/2.practice/1.docker",
+      },
+      {
+        text: "三.Docker基础",
+        link: "/base/3.knowledge/1.index",
+      },
+    ],
+  },
+  // {
+  //   text: '高级知识',
+  //   items: [
+  //     {
+  //       text: '一.组件精讲',
+  //       link: '/senior/use/1.recursionAndDynamics'
+  //     },
+  //   ]
+  // },
+  // {
+  //   text: '源码知识',
+  //   items: [
+  //     {
+  //       text: '1.vue源码解析',
+  //       link: '/source/vue/1.preparation'
+  //     },
+  //   ]
+  // }
+];
+module.exports = nav;
